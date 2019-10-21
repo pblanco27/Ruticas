06508c8155401bd3df9bdc90f31110b7c0fcc838
Gabriel <g.solorzano@estudiantec.cr>
2019-10-21 16:27:34 -0600

    Se agregaro la pantalla de asignar rutas.

--- Ruticas/asignarRuta.php
+++ Ruticas/asignarRuta.php
@@ -1,5 +1,7 @@
 <?php
-session_start(); ?>
+	include "conexion.php";
+	session_start();
+?>
 <!DOCTYPE HTML>
 <html lang="es">
 	<head>
@@ -89,7 +91,7 @@ session_start(); ?>
 									  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 										<ul>
 											<li>
-												<a class="dropdown-item" href="#">Crear empresa</a>
+												<a class="dropdown-item" href="crearEmpresa.php">Crear empresa</a>
 											</li>
 											<li>
 												<a class="dropdown-item" href="editarEmpresa.php">Editar empresa</a>
@@ -111,7 +113,7 @@ session_start(); ?>
 											</li>
 											<div class="dropdown-divider"></div>
 											<li>
-												<a class="dropdown-item" href="editarRuta.php">Editar rutas</a>
+												<a class="dropdown-item" href="#">Editar rutas</a>
 											</li>
 										</ul>
 									  </div>
@@ -138,112 +140,184 @@ session_start(); ?>
 
 		<section class="spacer green">
 			<div align="center">
-				<h2 class="pagetitle" style="color:white;">Crear empresa<h2>
+				<h2 class="pagetitle" style="color:white;">
+				Asignar rutas:
+				<h2>
 			</div>
 		</section>
-
 		<section id="maincontent" class="inner">
 			<div class="container">
 				<div class="row">
-					<div class="span8">
-						<!-- Aquí va el mapa -->
-						<div id="map">
-	            <script type="text/javascript" src="js/fun.js"></script>
-		        </div>
+					<div class="span4">
+						<center>
+							<div class="row">
+								<?php
+				        $sql = "call getEmpresasSimple()";
+				        $res = $conn->query($sql);
+				        ?>
+								<div class="span1">
+									Empresa:
+								</div>
+								<div class="span2">
+									<select name="empresa" id="empresa">
+										<option value="" style="display:none;">Seleccione una empresa</option>
+										<?php while ($row = $res->fetch_array()) {
+				            if (!empty($row['nombre'])) {?>
+				            <option value="<?php echo $row['idEmpresa']; ?>">
+				              <?php echo $row['nombre']; ?>
+				            </option>
+				            <?php }
+				            } ?>
+									</select>
+								</div>
+							</div>
+
+							<div class="row">
+								<?php
+								$res->close();
+								$conn->next_result();
+				        $sql = "call getRutasSimple()";
+				        $res = $conn->query($sql);
+				        ?>
+								<div class="span1">
+									Ruta:
+								</div>
+								<div class="span2">
+									<select>
+										<option value="" style="display:none;">Seleccione una empresa</option>
+										<?php while ($row = $res->fetch_array()) {
+				            if (!empty($row['numeroRuta'])) {?>
+				            <option value="<?php echo $row['idRuta']; ?>">
+				              <?php echo $row['numeroRuta']; ?>
+				            </option>
+				            <?php }
+				            } ?>
+									</select>
+								</div>
+							</div>
+					</center>
+					Costo Pasaje:
+					<input type="text">
+					<br>
+					Duracion del viaje (min):
+					<input type="text">
+					<br>
+					Unidades de discapacitados:
+					<input type="checkbox" class="custom-control-input" id="defaultUnchecked">
+					<br>
+					<br>
+						<div class="row">
+							<div class="span2">
+								<button type="button" class="btn" style="width:100%;" onclick="location.href='Scripts/cambiarEstadoRuta.php';" id='botonCambiarEstadoRuta'>-----</button>
+							</div>
+							<div class="span2">
+								<input type="submit" class="btn" style= "font-size: 16px; padding: 11px 19px;" value="Guardar">
+							</div>
+						</div>
 					</div>
 					<div class="span4">
-						<form id="register-form" action="Scripts/validarEmpresa.php" method="post" role="form" >
-							<div class="form-group" data-tip="El nombre debe ser de máximo 45 caracteres">
-								<input type="text" name="nombre" id="nombre" class="form-control" placeholder="Nombre de la empresa" maxlength="45" required>
+						<h2>Empresa</h2>
+							<div class="form-group" >
+								<input type="text" name="nombre" id="nombre" class="form-control" placeholder="Nombre de la empresa" maxlength="45" required readonly>
 								<font style="color:Red"><?php echo $_SESSION["error_nombre"]; ?></font>
 							</div>
-							<div class="form-group" data-tip="La zona donde opera debe ser de máximo 45 caracteres">
-								<input type="text" name="zona" id="zona" class="form-control" placeholder="Zona donde opera"  maxlength="45" required>
+							<div class="form-group">
+								<input type="text" name="zona" id="zona" class="form-control" placeholder="Zona donde opera"  maxlength="45" required readonly>
 								<font style="color:Red"><?php echo $_SESSION["error_zona"]; ?></font>
 							</div>
-							<div class="form-group" data-tip="La direccion debe ser de máximo 45 caracteres">
-								<input type="text" name="direccion" id="direccion" class="form-control" placeholder="Direccion física" maxlength="45" required>
+							<div class="form-group">
+								<input type="text" name="direccion" id="direccion" class="form-control" placeholder="Direccion física" maxlength="45" required readonly>
 								<font style="color:Red"><?php echo $_SESSION["error_direccion"]; ?></font>
 							</div>
-							<div class="form-group" data-tip="La latitud cambia según el punto elegido en el mapa">
-								<input type="text" name="latitud" id="latitud" class="form-control" placeholder="Latitud" readonly required>
+							<div class="form-group">
+								<input type="text" name="latitud" id="latitud" class="form-control" placeholder="Latitud" style="display:none;" readonly required>
 							</div>
-							<div class="form-group" data-tip="La longitud cambia según el punto elegido en el mapa">
+							<div class="form-group" style="display:none;">
 								<input type="text" name="longitud" id="longitud" class="form-control" placeholder="Longitud" readonly required>
 								<font style="color:Red"><?php echo $_SESSION["error_lats"]; ?></font>
 							</div>
-							<div class="form-group" data-tip="El teléfono debe contener números (a excepción del + del código de área), y debe ser de máximo 45 caracteres">
-								<input type="text" name="telefono" id="telefono" class="form-control" placeholder="Número telefónico" maxlength="45" required>
+							<div class="form-group">
+								<input type="text" name="telefono" id="telefono" class="form-control" placeholder="Número telefónico" maxlength="45" required readonly>
 								<font style="color:Red"><?php echo $_SESSION["error_telefono"]; ?></font>
 							</div>
-							<div class="form-group" data-tip="El correo debe contener letras (sin tildes) y números, un arroba y un dominio, de máximo 45 caracteres">
-								<input type="email" name="correo" id="correo" class="form-control" placeholder="Correo electrónico" maxlength="45" required>
+							<div class="form-group">
+								<input type="email" name="correo" id="correo" class="form-control" placeholder="Correo electrónico" maxlength="45" required readonly>
 								<font style="color:Red"><?php echo $_SESSION["error_correo"]; ?></font>
 							</div>
-							<div class="form-group" data-tip="Contacto ante eventualidad. Sigue el mismo formato del número telefónico">
-								<input type="text" name="contacto" id="contacto" class="form-control" placeholder="Contacto de emergencia" maxlength="45" required>
+							<div class="form-group" data-tip="Contacto ante eventualidad.">
+								<input type="text" name="contacto" id="contacto" class="form-control" placeholder="Contacto de emergencia" maxlength="45" required readonly>
 								<font style="color:Red"><?php echo $_SESSION["error_contacto"]; ?></font>
 							</div>
 
-							<div class="form-group" data-tip="Debe seleccionar a que hora empiezan a laborar">
-								Hora inicio: &nbsp;&nbsp;<select name="horaInicio" id="horaInicio" class="form-control" placeholder="Seleccione una hora" required>
-									<option value="1">1</option>
-									<option value="2">2</option>
-									<option value="3">3</option>
-									<option value="4">4</option>
-									<option value="5">5</option>
-									<option value="6">6</option>
-									<option value="7">7</option>
-									<option value="8">8</option>
-									<option value="9">9</option>
-									<option value="10">10</option>
-									<option value="11">11</option>
-									<option value="12">12</option>
-									<option value="13">13</option>
-									<option value="14">14</option>
-									<option value="15">15</option>
-									<option value="16">16</option>
-									<option value="17">17</option>
-									<option value="18">18</option>
-									<option value="19">19</option>
-									<option value="20">20</option>
-									<option value="21">21</option>
-									<option value="22">22</option>
-									<option value="23">23</option>
-									<option value="24">24</option>
-								</select>
-							</div>
-							<div class="form-group" data-tip="Debe seleccionar a que hora terminan de laborar">
-								Hora cierre: &nbsp;<select name="horaFin" id="horaFin" class="form-control" placeholder="Seleccione una hora" required>
-									<option value="1">1</option>
-									<option value="2">2</option>
-									<option value="3">3</option>
-									<option value="4">4</option>
-									<option value="5">5</option>
-									<option value="6">6</option>
-									<option value="7">7</option>
-									<option value="8">8</option>
-									<option value="9">9</option>
-									<option value="10">10</option>
-									<option value="11">11</option>
-									<option value="12">12</option>
-									<option value="13">13</option>
-									<option value="14">14</option>
-									<option value="15">15</option>
-									<option value="16">16</option>
-									<option value="17">17</option>
-									<option value="18">18</option>
-									<option value="19">19</option>
-									<option value="20">20</option>
-									<option value="21">21</option>
-									<option value="22">22</option>
-									<option value="23">23</option>
-									<option value="24">24</option>
-								</select>
-							</div>
-
 							<div class="form-group">
-								<input type="submit" class="form-control btn btn-register" value="Crear">
+								Hora inicio: &nbsp;&nbsp;<select name="horaInicio" id="horaInicio" class="form-control" placeholder="Seleccione una hora" required readonly>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+									<option value="11">11</option>
+									<option value="12">12</option>
+									<option value="13">13</option>
+									<option value="14">14</option>
+									<option value="15">15</option>
+									<option value="16">16</option>
+									<option value="17">17</option>
+									<option value="18">18</option>
+									<option value="19">19</option>
+									<option value="20">20</option>
+									<option value="21">21</option>
+									<option value="22">22</option>
+									<option value="23">23</option>
+									<option value="24">24</option>
+								</select>
+							</div>
+							<div class="form-group">
+								Hora cierre: &nbsp;<select name="horaFin" id="horaFin" class="form-control" placeholder="Seleccione una hora" required readonly>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+									<option value="11">11</option>
+									<option value="12">12</option>
+									<option value="13">13</option>
+									<option value="14">14</option>
+									<option value="15">15</option>
+									<option value="16">16</option>
+									<option value="17">17</option>
+									<option value="18">18</option>
+									<option value="19">19</option>
+									<option value="20">20</option>
+									<option value="21">21</option>
+									<option value="22">22</option>
+									<option value="23">23</option>
+									<option value="24">24</option>
+								</select>
+							</div>
+					</div>
+					<div class="span4">
+						<h2>Ruta</h2>
+						<form id="register-form" action="Scripts/modificarRuta.php" method="post" role="form" >
+							<div class="form-group" data-tip="El número de ruta debe ser de máximo 45 caracteres">
+								<input type="text" name="numero" id="numero" class="form-control" placeholder="Número de la ruta" maxlength="45" required readonly>
+								<font style="color:Red"><?php echo $_SESSION["error_nombre"]; ?></font>
+							</div>
+							<div class="form-group" data-tip="La descripción debe ser de máximo 250 caracteres" >
+								<textarea name="descripcion" id="descripcion" rows="7" placeholder="Descripción del ruta" style="resize: none;" maxlength="250" required readonly></textarea>
+								<font style="color:Red"><?php echo $_SESSION["error_descripcion"]; ?></font>
+							</div>
+								</div>
 							</div>
 						</form>
 					</div>
@@ -289,5 +363,8 @@ session_start(); ?>
 		<script src="js/animate.js"></script>
 		<script src="js/jquery.tweet.js"></script>
 		<script src="js/custom.js"></script>
+		<script type="text/javascript" src="js/comboBoxEmpresa.js"></script>
+
 	</body>
+
 </html>
